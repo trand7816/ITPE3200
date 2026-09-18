8aaf58fe8265ccc7a869a8127f485d7df270b6e9
Miwaa005 <miwaa00555@gmail.com>
2026-09-18 15:32:46 +0200

    Laget en mvc-modell

---
+++ itpe3200/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using itpe3200.Models;
+
+namespace itpe3200.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ itpe3200/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace itpe3200.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
